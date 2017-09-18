0f0fd915181607919201bba51421b123d705557f
Timothy King <timothyking@Timothys-MacBook-Pro.local>
2017-09-18 10:35:09 -0700

    Functions Playground Complete

---
+++ Pages/11-Create-Something-Cool.xcplaygroundpage/Contents.swift
@@ -0,0 +1,153 @@
+/*:
+ ![Make School Banner](./swift_banner.png)
+ # Create something cool
+ 
+ - experiment: Use everything you learned to draw something awesome! See below for a few more functions you can use to bring your art to life!
+ 
+ ## Drop and lift the pen
+ 
+ - callout(Drop the pen): `penDown()`
+ 
+ 
+ - callout(Lift the pen): `penUp()`
+ 
+ 
+ - note: The pen starts out down.
+ 
+ ## Moving the pen
+ 
+ - callout(Move by a number of steps): `move(steps: Int)`
+ 
+ 
+ - callout(Move directly to coordinates): `moveTo(x: Int, y: Int)`
+ 
+ 
+ - callout(Rotate heading by an angle): `rotate(degrees: Int)`
+ 
+ ## Change the pen
+ 
+ - callout(Set color of pen): `setColor(red: Double, green: Double, blue: Double)`
+ 
+ 
+ - note: Each value should be between 0 and 1. eg. `setColor(red: 0.0, green: 0.0, blue: 0.0)` would change the pen to be black. `setColor(red: 1.0, green: 0.0, blue: 0.0)` would change the pen to be red.
+ 
+ 
+ - callout(Set thickness of pen): `set(thickness: Double)`
+ 
+ */
+//: [Previous](@previous)
+/*:
+ 
+ - callout(Challenge): Draw something awesome in the space below!
+ 
+ */
+func upAndDown(moving: String){
+    if moving == "up"{
+        penUp()
+    } else if moving == "down" {
+        penDown()
+    }
+}
+
+
+func setColor(red: Double, green: Double, blue:Double){
+}
+
+func setThickness(thickness: Double){
+}
+
+func drawPolygon(numberofSides: Int, sideLength:Int){
+    for  _ in 0...numberofSides{
+        move(steps: sideLength)
+        rotate(degrees: 360 / numberofSides)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: This is setup code to make the live visualization work!
+Pen.delay = 0.1
+import PlaygroundSupport
+import Foundation
+let results = DrawingScene.setup()
+PlaygroundPage.current.liveView = results
